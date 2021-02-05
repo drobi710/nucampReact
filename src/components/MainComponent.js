e8c1bd67521ea60cfdd92ac8ba670bc422955b88
Danielle Robitshek <drobi710@gmail.com>
2021-02-05 16:18:26 -0500

    add ContactComponent and route in MainComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 
@@ -29,8 +30,10 @@ class Main extends Component {
                 <Switch>
                     {/* any traffic going to home goes to the HomePage Component */}
                     <Route path='/home' component={HomePage} />;
-                    {/* kind of like case keywords in JS switch statement */}
+                    {/* kind of like case keywords in JS switch statement if passing state data as props good to use render */}
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    {/* telling our App to watch the browser address bar and whenever the Route matches contactus then show Contact component */}
+                    <Route exact path='/contactus' component={Contact} />
                     {/* Redirect acts as a catch all kinda like default in switch statement */}
                     <Redirect to='/home' />
                 </Switch>
